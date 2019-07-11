8169136f183c1ac727873d0f6361cc45131eb7fa
TheRandMan <33759959+TheRandMan@users.noreply.github.com>
2019-07-10 21:04:15 -0700

    Update 00003-add-swype-library.sh

--- 00003-add-swype-library.sh
+++ 00003-add-swype-library.sh
@@ -3,5 +3,5 @@
 SWYPE="https://github.com/TheRandMan/swype_for_rattlesnakeos/raw/master/libjni_latinime.so"
 mkdir -p $BUILD_DIR/swype
 echo "Downloading swype $SWYPE"
-wget -O $BUILD_DIR/mods/libjni_latinime.so $SWYPE
-sed -i "\$aPRODUCT_COPY_FILES += mods/libjni_latinime.so:system/lib64/libjni_latinime.so" ${BUILD_DIR}/build/make/target/product/core.mk
+wget -O $BUILD_DIR/swype/libjni_latinime.so $SWYPE
+sed -i "\$aPRODUCT_COPY_FILES += swype/libjni_latinime.so:system/lib64/libjni_latinime.so" ${BUILD_DIR}/build/make/target/product/core.mk
